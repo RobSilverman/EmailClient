bc456550394625ab43a621a5dca27b7cb1f8d236
Rob Silverman <robert.silverman@willowtreeapps.com>
2020-07-11 16:49:50 -0400

    Initial Commit

---
+++ EmailClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmailClient
+//
+//  Created by Robert Silverman on 7/11/20.
+//  Copyright © 2020 Robert Silverman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
