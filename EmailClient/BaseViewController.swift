15a91e12dcc78fff0970dee7b46f0a9bff7384b1
Rob Silverman <robert.silverman@willowtreeapps.com>
2020-07-14 17:20:26 -0400

    progress so far?

--- EmailClient/BaseViewController.swift
+++ EmailClient/BaseViewController.swift
@@ -12,9 +12,20 @@ class BaseViewController: UIViewController {
 
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var tableViewContainer: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+    
+    func setupViews() {
+        searchBar.layer.cornerRadius = searchBar.frame.height / 2
+        tableViewContainer.layer.cornerRadius = 10.0
+        if let searchText = searchBar.value(forKey: "searchField") as? UITextField {
+            searchText.backgroundColor = UIColor(red: 0.44, green: 0.61, blue: 0.89, alpha: 1)
+            searchText.textColor = UIColor.white
+        }
     }
 
 
