70dacc634533972b7dec4ddde1647f873ed9747e
Rob Silverman <robert.silverman@willowtreeapps.com>
2020-07-15 10:46:46 -0400

    round corners of tableview

--- EmailClient/BaseViewController.swift
+++ EmailClient/BaseViewController.swift
@@ -21,7 +21,8 @@ class BaseViewController: UIViewController {
     
     func setupViews() {
         searchBar.layer.cornerRadius = searchBar.frame.height / 2
-        tableViewContainer.layer.cornerRadius = 10.0
+        tableViewContainer.clipsToBounds = true
+        tableViewContainer.layer.cornerRadius = 20.0
         if let searchText = searchBar.value(forKey: "searchField") as? UITextField {
             searchText.backgroundColor = UIColor(red: 0.44, green: 0.61, blue: 0.89, alpha: 1)
             searchText.textColor = UIColor.white
